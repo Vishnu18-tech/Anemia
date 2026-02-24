1555ad92ced6ad36917bea37e85991a7c5fd9822
Vishnu <vishnu.joshi4045@gmail.com>
2026-02-24 14:31:56 +0530

    updated

--- main.py
+++ main.py
@@ -14,14 +14,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler,OneHotEncoder
-from sklearn.metrics import accuracy_score,classification_report,roc_auc_score,roc_curve
+from sklearn.metrics import accuracy_score,classification_report,roc_auc_score,roc_curve,f1_score
 X=df.drop(columns=['Anaemic'],axis=1)
 y=df['Anaemic'].map({'No' : 0,'Yes' : 1})
-print(y.value_counts())
 catcols=X.select_dtypes(include='object').columns
 numcols=X.select_dtypes(include='float64').columns
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
-import math
+#import math
 #print(math.sqrt(len(y_train)))
 preprocessing=ColumnTransformer(
     transformers=[
@@ -37,8 +36,9 @@ model.fit(X_train,y_train)
 y_pred=model.predict(X_test)
 y_proba=model.predict_proba(X_test)[:,1]
 
-print(accuracy_score(y_test,y_pred))
-print(classification_report(y_test,y_pred))
+print(f'Accuracy : {accuracy_score(y_test,y_pred)}')
+print(f'Classification Report : \n{classification_report(y_test,y_pred)}')
+print(f'F1 Score : {f1_score(y_test,y_pred)}')
 fpr,tpr,thresholds=roc_curve(y_test,y_proba)
 plt.figure()
 plt.plot(fpr,tpr,label='area = %0.2f'%roc_auc_score(y_test,y_proba))
